f3064a2b7b2dd66b5daaede0052626a5929c5847
Christofer Hammarback <khammarback@gmail.com>
2024-10-23 14:15:27 +0200

    First commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+   res.render("index", { title: "Freaky Fashion" });
+});
+
+router.get("/checkout", function (req, res, next) {
+   res.render("checkout", { title: "Checkout" });
+});
+
+router.get("/product-details", function (req, res, next) {
+   res.render("product-details", { title: "Prodouct-Details" });
+});
+
+router.get("/admin/products/index", function (req, res, next) {
+   res.render("admin/products/index", { title: "Admin Index" });
+});
+
+router.get("/admin/products/new", function (req, res, next) {
+   res.render("admin/products/new", { title: "New Product" });
 });
 
 module.exports = router;
